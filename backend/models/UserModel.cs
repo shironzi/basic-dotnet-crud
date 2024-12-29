5235a0bfa009b5392d2edd748bac57a725d69dfc
shironzi <aaronbaon1@gmail.com>
2024-12-30 05:13:05 +0800

    Add User model with properties for Username, Email, Password, FirstName, and LastName

---
+++ backend/models/UserModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+public class User : BaseEntityModel
+{
+
+    [StringLength(50)]
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+}
