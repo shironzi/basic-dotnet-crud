fbdd16f474ab1ef8e94072ffe3f8c46bb536c68e
shironzi <aaronbaon1@gmail.com>
2025-01-02 08:53:35 +0800

    refactor: update the property Name into Title

--- backend/Models/TodoItem.cs
+++ backend/Models/TodoItem.cs
@@ -6,6 +6,6 @@ namespace backend.Models;
 public class TodoItem : BaseModel
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
+    public required string Title { get; set; }
     public required string IsComplete { get; set; }
 }
