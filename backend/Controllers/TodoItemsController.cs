4f951f26430b52aa3fcafbd5894794d415a3474d
shironzi <aaronbaon1@gmail.com>
2025-01-03 11:18:36 +0800

    feat: add crud operations and update route

--- backend/Controllers/TodoItemsController.cs
+++ backend/Controllers/TodoItemsController.cs
@@ -1,15 +1,40 @@
+using backend.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class TodoItemsController : Controller
     {
         [HttpGet]
         public IActionResult Index()
         {
-            return Ok("This is my default action...");
+            return Ok("get all todos");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTodoItemById([FromQuery] int id)
+        {
+            return Ok("get todo item by id");
+        }
+
+        [HttpPost]
+        public IActionResult CreateTodo([FromBody] string title)
+        {
+            return Ok("Todo Item has been created successfully!");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateTodoItem([FromQuery] int id, [FromBody] TodoItem todoItem)
+        {
+            return Ok("Todo has been updated successfully!");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTodoItem([FromQuery] int id)
+        {
+            return Ok("Todo with ${id} has been deleted!");
         }
     }
 }
